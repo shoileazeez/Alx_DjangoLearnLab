2e5beb87c225de31904113e07986c416b6ac3322
shoile azeez <shoabdulazeez@gmail.com>
2024-09-06 20:37:49 +0100

    Building Custom Views and Generic Views in Django REST Framework

---
+++ advanced-api-project/api/urls.py
@@ -0,0 +1,12 @@
+# bookshelf/urls.py
+
+from django.urls import path
+from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
+
+urlpatterns = [
+    path('books/', BookListView.as_view(), name='book-list'),  # List all books
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),  # Detail view for a single book
+    path('books/new/', BookCreateView.as_view(), name='book-create'),  # Create a new book
+    path('books/<int:pk>/edit/', BookUpdateView.as_view(), name='book-update'),  # Update an existing book
+    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),  # Delete a book
+]
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.views.generic import DetailView
+from django.views.generic import CreateView
+from django.views.generic import UpdateView
+from django.views.generic import DeleteView
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from django.urls import reverse_lazy
+from .models import Book
 
+class BookListView(ListView):
+    model = Book
+    template_name = 'bookshelf/book_list.html'  # Define your template
+    context_object_name = 'books'
+    permission_classes = [AllowAny]
+
+class BookDetailView(DetailView):
+    model = Book
+    template_name = 'bookshelf/book_detail.html'  # Template for displaying a single book
+    context_object_name = 'book'
+    permission_classes = [AllowAny]
+    
+class BookCreateView(CreateView):
+    model = Book
+    fields = ['title', 'author', 'publication_year']  # Fields to show in the form
+    template_name = 'bookshelf/book_form.html'  # Template for creating a book
+    success_url = reverse_lazy('book-list')
+    permission_classes = [IsAuthenticated]
+    
+class BookUpdateView(UpdateView):
+    model = Book
+    fields = ['title', 'author', 'publication_year']  # Fields to edit
+    template_name = 'bookshelf/book_form.html'  # Template for updating a book
+    success_url = reverse_lazy('book-list') 
+    permission_classes = [IsAuthenticated]
+      
+class BookDeleteView(DeleteView):
+    model = Book
+    template_name = 'bookshelf/book_confirm_delete.html'  # Template for confirming deletion
+    success_url = reverse_lazy('book-list') 
+    permission_classes = [IsAuthenticated]         
 # Create your views here.
