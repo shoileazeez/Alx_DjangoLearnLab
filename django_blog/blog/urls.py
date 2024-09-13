ced4de30c43af20ba19d5c7d844a4cb7569b4c0a
shoile azeez <shoabdulazeez@gmail.com>
2024-09-13 13:07:57 +0100

    Implementing the Blog's User Authentication System

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -8,10 +8,10 @@ urlpatterns = [
     path('profile/', profile, name='profile_update'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('posts/', List_view.as_view(), name='post_list'),       # List all posts
-    path('posts/<int:pk>/', Detail_View.as_view(), name='post_detail'),  # View a specific post
-    path('posts/new/', create_view.as_view(), name='post_create'),  # Create a new post
-    path('posts/<int:pk>/edit/', Update_View.as_view(), name='post_edit'),  # Edit an existing post
-    path('posts/<int:pk>/delete/', Delete_view.as_view(), name='post_delete'),  # Delete a post
+    path('post/', List_view.as_view(), name='post_list'),       # List all posts
+    path('post/<int:pk>/', Detail_View.as_view(), name='post_detail'),  # View a specific post
+    path('post/new/', create_view.as_view(), name='post_create'),  # Create a new post
+    path('post/<int:pk>/update/', Update_View.as_view(), name='post_edit'),  # Edit an existing post
+    path('post/<int:pk>/delete/', Delete_view.as_view(), name='post_delete'),  # Delete a post
     
 ]
