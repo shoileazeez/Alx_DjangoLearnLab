055c92fa6e5623e77d9e15e3f61e275190a42bd2
shoile azeez <shoabdulazeez@gmail.com>
2024-09-15 03:09:38 +0100

    my_blog

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -7,4 +7,6 @@ class Post(models.Model):
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    
+ 
+ 
+class comment(models.Model): 
