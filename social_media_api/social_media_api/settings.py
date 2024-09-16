8849a9d40fba971196b95ec753bd9f7afa9dd6c7
shoile azeez <shoabdulazeez@gmail.com>
2024-09-16 21:48:09 +0100

    Project Setup and User Authentication for a Social Media API

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -127,7 +127,7 @@ USE_I18N = True
 USE_TZ = True
 
 # settings.py
-AUTH_USER_MODEL = 'accounts.SocialApp'
+AUTH_USER_MODEL = 'accounts.CustomUser'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
